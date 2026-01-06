51111fb19601d80e362505f33869f68cc10c9fb7
원우연 <won-uyeon@won-uyeon-ui-MacBookPro.local>
2026-01-06 23:09:26 +0900

    fix: Remove buggy absolute borders from Dialog component to fix Black Line glitch

--- components/ui/8bit/dialog.tsx
+++ components/ui/8bit/dialog.tsx
@@ -72,15 +72,6 @@ function DialogContent({
       {...props}
     >
       {children}
-
-      <div
-        className="absolute inset-0 border-x-6 -mx-1.5 border-foreground dark:border-ring pointer-events-none"
-        aria-hidden="true"
-      />
-      <div
-        className="absolute inset-0 border-y-6 -my-1.5 border-foreground dark:border-ring pointer-events-none"
-        aria-hidden="true"
-      />
     </ShadcnDialogContent>
   );
 }
